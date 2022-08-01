d69e0aae20757060024f5a750e08d4e533def15e
m3thom <39703347+m3thom@users.noreply.github.com>
2022-08-01 16:57:19 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ionic.splash.screen',
+  appName: 'ionic-splash-screen',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
